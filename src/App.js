1280a27e54bd9748796859b1a5309d7506f67655
TAMILVANANSUBRAMANIYAN <153979173+TAMILVANANSUBRAMANIYAN@users.noreply.github.com>
2024-09-03 11:52:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
+ 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [userData, setUserData] = useState(null);
+
+
+    const handleInputvalue=(e)=>
+      {
+         setInputValue(e.target.value)
+      }
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();    
+          const userId = inputValue.replace('user', '');
+          const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+          setUserData(response.data);
+      };
+     
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='navlinks'>
+      <ul className='navbar'>
+        <li>Gmail</li>
+        <li>Images</li>
+      </ul>
     </div>
+
+    <h1 className='sitename'>Google</h1>
+    <div className='centerinput'>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="inputbox"
+          type='text'
+          placeholder='Enter your URL (e.g., user1)'
+          value={inputValue}
+          onChange={handleInputvalue}
+        />
+      </form>
+    </div>
+
+    {userData && (
+      <div className="user-details">
+        <h2>User Details:</h2>
+        <p>Name: {userData.name}</p>
+        <p>Name: {userData.id}</p>
+      </div>
+    )}
+  </div>
   );
 }
 
