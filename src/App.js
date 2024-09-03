042a84764cadf6d14943f02a50ea2528f9afdabd
TAMILVANANSUBRAMANIYAN <153979173+TAMILVANANSUBRAMANIYAN@users.noreply.github.com>
2024-09-03 11:58:22 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,10 @@
-import { useState } from 'react';
-import './App.css';
-import axios from 'axios';
+import Interviewtask from "./login";
 
- 
 function App() {
-  const [inputValue, setInputValue] = useState('');
-  const [userData, setUserData] = useState(null);
-
-
-    const handleInputvalue=(e)=>
-      {
-         setInputValue(e.target.value)
-      }
-
-      const handleSubmit = async (e) => {
-        e.preventDefault();    
-          const userId = inputValue.replace('user', '');
-          const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
-          setUserData(response.data);
-      };
-     
-  
   return (
     <div className="App">
-    <div className='navlinks'>
-      <ul className='navbar'>
-        <li>Gmail</li>
-        <li>Images</li>
-      </ul>
+       <Interviewtask/>
     </div>
-
-    <h1 className='sitename'>Google</h1>
-    <div className='centerinput'>
-      <form onSubmit={handleSubmit}>
-        <input
-          className="inputbox"
-          type='text'
-          placeholder='Enter your URL (e.g., user1)'
-          value={inputValue}
-          onChange={handleInputvalue}
-        />
-      </form>
-    </div>
-
-    {userData && (
-      <div className="user-details">
-        <h2>User Details:</h2>
-        <p>Name: {userData.name}</p>
-        <p>Name: {userData.id}</p>
-      </div>
-    )}
-  </div>
   );
 }
 
---
+++ src/login.js
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import './login.css';
+import axios from 'axios';
+
+ 
+function Interviewtask() {
+  const [inputValue, setInputValue] = useState('');
+  const [userData, setUserData] = useState(null);
+
+
+    const handleInputvalue=(e)=>
+      {
+         setInputValue(e.target.value)
+      }
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();    
+          const userId = inputValue.replace('user', '');
+          const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+          setUserData(response.data);
+      };
+     
+  
+  return (
+    <div className="App">
+    <div className='navlinks'>
+      <ul className='navbar'>
+        <li>Gmail</li>
+        <li>Images</li>
+      </ul>
+    </div>
+
+    <h1 className='sitename'>Google</h1>
+    <div className='centerinput'>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="inputbox"
+          type='text'
+          placeholder='Enter your URL (e.g., user1)'
+          value={inputValue}
+          onChange={handleInputvalue}
+        />
+      </form>
+    </div>
+
+    {userData && (
+      <div className="user-details">
+        <h2>User Details:</h2>
+        <p>Name: {userData.name}</p>
+        <p>Name: {userData.id}</p>
+      </div>
+    )}
+  </div>
+  );
+}
+
+export default Interviewtask;
